bde632e4b67c8eaf6386701c17ddaca027d0e08e
KAthousand <KAthousand@gmail.com>
2021-05-29 15:47:59 -0400

    changed menu to photo grid layout, added menu button svg

--- client/src/screens/Menu/Menu.jsx
+++ client/src/screens/Menu/Menu.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import MenuButton from '../../components/MenuButton/MenuButton';
 import Slider from '../../components/Slider/Slider';
 import "./Menu.css"
 
 function Menu(props) {
   return (
     <div className='menu-container' id='menu'>
-      <Slider />
+      <div className='grid'>
+        <div className='top-left photo'></div>
+        <div className='top-center photo'></div>
+        <div className='bottom-left photo'></div>
+        <div className='bottom-center photo'></div>
+      </div>
+      <div className='bottom-right photo'></div>
+      <div className='menu-btn-container'>
+        <MenuButton/>
+      </div>
     </div>
   );
 }
