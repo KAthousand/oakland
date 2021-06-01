62be2d0d7f7e5b54591e0ca45840633bc37bff29
KAthousand <KAthousand@gmail.com>
2021-06-01 15:02:16 -0400

    added logo to header

--- client/src/screens/shared/Header/Header.jsx
+++ client/src/screens/shared/Header/Header.jsx
@@ -7,14 +7,14 @@ function Header(props) {
   return (
     <div className='header-container'>
       <div className='header-content'>
-      <a href='#title' className='logo'  alt='home'> </a>
+      <a href='#title' className='nav-logo'><p>the oakland</p> </a>
         <a href='#menu' className='nav-link'>MENU</a>
         <a href='#info' className='nav-link'>LOCATION &amp; HOURS</a>
         <a href='https://app.upserve.com/b/the-oakland/gift-card ' className='nav-link'>GIFT CARDS</a>
         <a href='https://www.exploretock.com/theoakland' className='nav-link reservation'>RESERVATIONS</a>
       </div>
       <div className='nav-menu'>
-        <div className='logo'></div>
+      <a href='#title' className='nav-logo-mobile'><p>the oakland</p></a>
         <div className={navOpen ? 'nav-icon2' : 'nav-icon'}>
           <button onClick={() => setNavOpen(!navOpen)}></button>
         </div>
