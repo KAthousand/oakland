1981aca7d3582d3670a3c622e43f54551da037c4
kthousand <kthousand@jacapps.com>
2021-10-12 21:58:16 -0400

    fixing mobile nav + hours

--- client/src/screens/Information/Information.jsx
+++ client/src/screens/Information/Information.jsx
@@ -1,38 +1,70 @@
-import React from 'react';
-import './Information.css'
+import React from "react"
+import "./Information.css"
 
 function Information(props) {
   return (
-    <div className='info-container' id='info'>
-      <div className='info-content'>
-        <div className='info-text'>
-          <div className='info-header'><h5>LOCATION</h5></div>
-          <div className='info-body'><p>201 W. 9 MILE RD, FERNDALE, MI 48220</p></div>
-          <a href='https://www.google.com/maps?q=201+W+9+Mile+Rd,+Ferndale,+MI&hl=en&sll=45.00109,-86.270553&sspn=14.885097,29.355469&oq=201+W+9+Mile+&hnear=201+W+9+Mile+Rd,+Ferndale,+Michigan+48220&t=m&z=17' target="_blank" rel="noreferrer">
-            <button className='map-btn'>MAP</button>
+    <div className="info-container" id="info">
+      <div className="info-content">
+        <div className="info-text">
+          <div className="info-header">
+            <h5>LOCATION</h5>
+          </div>
+          <div className="info-body">
+            <p>201 W. 9 MILE RD, FERNDALE, MI 48220</p>
+          </div>
+          <a
+            href="https://www.google.com/maps?q=201+W+9+Mile+Rd,+Ferndale,+MI&hl=en&sll=45.00109,-86.270553&sspn=14.885097,29.355469&oq=201+W+9+Mile+&hnear=201+W+9+Mile+Rd,+Ferndale,+Michigan+48220&t=m&z=17"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="map-btn">MAP</button>
           </a>
-          <div className='info-header'><h5>HOURS</h5></div>
-          <div className='info-body'>
-
+          <div className="info-header">
+            <h5>HOURS</h5>
+          </div>
+          <div className="info-body">
             {/* THIS IS WHERE YOU WOULD EDIT THE HOURS */}
-            <p>TUES - THURS: 5<span className='hours'>pm</span>-12:30<span className='hours'>am</span></p>
-            <p>FRI - SAT: 5<span className='hours'>pm</span>-1:30<span className='hours'>am</span></p>
-            <p>SUNDAY: 5<span className='hours'>pm</span>-12:30<span className='hours'>am</span></p>
+            <p>
+              TUES - SAT: 5<span className="hours">pm</span>-12:00
+              <span className="hours">am</span>
+            </p>
+            {/* <p>FRI - SAT: 5<span className='hours'>pm</span>-11:00<span className='hours'>pm</span></p> */}
+            <p>
+              SUNDAY: 4<span className="hours">pm</span>-11:00
+              <span className="hours">pm</span>
+            </p>
           </div>
 
           {/* THIS IS WHERE YOU WOULD EDIT CONTACT INFORMATION */}
-          <div className='info-header'><h5>CONTACT</h5></div>
-          <div className='info-body'><p>(248)291-5295</p>
-            <a href="mailto:info@theoaklandferndale.com" className='email'>info@theoaklandferndale.com</a>
+          <div className="info-header">
+            <h5>CONTACT</h5>
           </div>
-          <div className= 'icon-container'>
-            <a href='http://www.facebook.com/ArtNoveltyCompany' target="_blank" rel="noreferrer"><i className='fab fa-facebook-square'></i></a>
-            <a href='http://www.instagram.com/artnoveltycompany' target="_blank" rel="noreferrer"><i className='fab fa-instagram-square'></i></a>
+          <div className="info-body">
+            <p>(248)291-5295</p>
+            <a href="mailto:info@theoaklandferndale.com" className="email">
+              info@theoaklandferndale.com
+            </a>
+          </div>
+          <div className="icon-container">
+            <a
+              href="http://www.facebook.com/ArtNoveltyCompany"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-facebook-square"></i>
+            </a>
+            <a
+              href="http://www.instagram.com/artnoveltycompany"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-instagram-square"></i>
+            </a>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Information;
+export default Information
--- client/src/screens/shared/Header/Header.jsx
+++ client/src/screens/shared/Header/Header.jsx
@@ -1,35 +1,78 @@
-import {useState} from 'react';
+import { useState } from "react"
 import "./Header.css"
 
 function Header(props) {
   const [navOpen, setNavOpen] = useState(false)
 
   return (
-    <div className='header-container'>
-      <div className='header-content'>
-      <a href='#title' className='nav-logo'><p>the oakland</p> </a>
-        <a href='#menu' className='nav-link'>MENU</a>
-        <a href='#info' className='nav-link'>LOCATION &amp; HOURS</a>
-        <a href='https://app.upserve.com/b/the-oakland/gift-card ' className='nav-link'>GIFT CARDS</a>
-        <a href='https://www.exploretock.com/theoakland' className='nav-link reservation'>RESERVATIONS</a>
+    <div className="header-container">
+      <div className="header-content">
+        <a href="#title" className="nav-logo">
+          <p>the oakland</p>{" "}
+        </a>
+        <a href="#menu" className="nav-link">
+          MENU
+        </a>
+        <a href="#info" className="nav-link">
+          LOCATION &amp; HOURS
+        </a>
+        <a
+          href="https://app.upserve.com/b/the-oakland/gift-card "
+          className="nav-link"
+        >
+          GIFT CARDS
+        </a>
+        <a
+          href="https://www.exploretock.com/theoakland"
+          className="nav-link reservation"
+        >
+          RESERVATIONS
+        </a>
       </div>
-      <div className='nav-menu'>
-      <a href='#title' className='nav-logo-mobile'><p>the oakland</p></a>
-        <div className={navOpen ? 'nav-icon2' : 'nav-icon'}>
+      <div className="nav-menu">
+        <a href="#title" className="nav-logo-mobile">
+          <p>the oakland</p>
+        </a>
+        <div className={navOpen ? "nav-icon2" : "nav-icon"}>
           <button onClick={() => setNavOpen(!navOpen)}></button>
         </div>
       </div>
-      <div className={navOpen ? 'mobile-nav' : 'mobile-nav-closed'}>
-        <div className={navOpen ? 'mobile-nav-content' : 'mobile-nav-content-closed'}>
-        <a href='_blank' className={navOpen ? 'mobile-link' : 'mobile-link-closed'}>ORDER ONLINE</a>
-          <a href='/menu' className={navOpen ? 'mobile-link' : 'mobile-link-closed'}>MENU</a>
-          <a href='/info' className={navOpen ? 'mobile-link' : 'mobile-link-closed'}>LOCATION &amp; HOURS</a>
-          <a href='_blank' className={navOpen ? 'mobile-link' : 'mobile-link-closed'}>PRIVATE EVENTS</a>
-          <a href='_blank' className={navOpen ? 'mobile-link reservation' : 'mobile-link-closed'}>RESERVATIONS</a>
+      <div className={navOpen ? "mobile-nav" : "mobile-nav-closed"}>
+        <div
+          className={
+            navOpen ? "mobile-nav-content" : "mobile-nav-content-closed"
+          }
+        >
+          <a
+            href="#menu"
+            className={navOpen ? "mobile-link" : "mobile-link-closed"}
+          >
+            MENU
+          </a>
+          <a
+            href="#info"
+            className={navOpen ? "mobile-link" : "mobile-link-closed"}
+          >
+            LOCATION &amp; HOURS
+          </a>
+          <a
+            href="https://app.upserve.com/b/the-oakland/gift-card "
+            className={navOpen ? "mobile-link" : "mobile-link-closed"}
+          >
+            GIFT CARDS
+          </a>
+          <a
+            href="https://www.exploretock.com/theoakland"
+            className={
+              navOpen ? "mobile-link reservation" : "mobile-link-closed"
+            }
+          >
+            RESERVATIONS
+          </a>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
